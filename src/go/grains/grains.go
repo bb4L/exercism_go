627c5516e093caaebc54957fa3cf4de1e93b6469
bb4L <39266013+bb4L@users.noreply.github.com>
2020-08-13 13:11:29 +0200

    Add solution for grains

---
+++ src/go/grains/grains.go
@@ -0,0 +1,19 @@
+package grains
+
+import (
+	"errors"
+)
+
+// Square return value on field n
+func Square(n int) (uint64, error) {
+	if n < 1 || n > 64 {
+		return uint64(0), errors.New("Wrong input should be in range 1 to 64 including")
+	}
+	return uint64(1 << (n - 1)), nil
+}
+
+// Total return the total of the square
+func Total() uint64 {
+	k := uint64(1 << 32)
+	return k*k - 1
+}
