a7086b29b633b21bcafe8bb4a08830d5ffccaefa
bb4L <39266013+bb4L@users.noreply.github.com>
2020-08-22 08:35:25 +0200

    Add solution for sieve

---
+++ src/go/sieve/sieve.go
@@ -0,0 +1,21 @@
+package sieve
+
+// Sieve returns all the primenumbers up to the limit
+func Sieve(limit int) (result []int) {
+	vals := make([]bool, limit-1)
+	min := 2
+
+	for i := 2; i <= limit; i++ {
+		if vals[i-min] == false {
+			result = append(result, i)
+		}
+		if 2*i > limit {
+			continue
+		}
+		for k := 2 * i; k <= limit; k = k + i {
+			vals[k-min] = true
+		}
+	}
+
+	return
+}
