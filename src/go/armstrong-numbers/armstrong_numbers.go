71ed50f1a50b5a6957f18a6c249ed976e0967873
bb4L <39266013+bb4L@users.noreply.github.com>
2020-08-25 21:48:25 +0200

    Improve armstrong_numbers now with own in pow func

--- src/go/armstrong-numbers/armstrong_numbers.go
+++ src/go/armstrong-numbers/armstrong_numbers.go
@@ -1,16 +1,23 @@
 package armstrong
 
 import (
-	"math"
 	"strconv"
 )
+
+func pow(a, b int) (result int) {
+	result = 1
+	for i := 1; i <= b; i++ {
+		result *= a
+	}
+	return
+}
 
 // IsNumber checks if the given number is a armstrong number
 func IsNumber(number int) bool {
 	length := len(strconv.Itoa(number))
-	summedNumber := float64(0)
+	summedNumber := 0
 	for i := 0; i < length; i++ {
-		summedNumber += math.Pow(float64(number/int(math.Pow(10, float64(i)))%10), float64(length))
+		summedNumber += pow((number/pow(10, i))%10, length)
 	}
 	return number == int(summedNumber)
 }
