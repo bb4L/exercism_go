ee813e26df7601b33946de074a72e7ea76fa0c02
bb4L <39266013+bb4L@users.noreply.github.com>
2020-08-29 12:57:22 +0200

    Add first running solution to tree-building

--- src/go/tree-building/tree_building.go
+++ src/go/tree-building/tree_building.go
@@ -1,6 +1,9 @@
 package tree
 
-import "errors"
+import (
+	"errors"
+	"sort"
+)
 
 // Record represents a record to setup the tree
 type Record struct {
@@ -20,39 +23,51 @@ func Build(data []Record) (*Node, error) {
 		return nil, nil
 	}
 
-	m := make(map[int]Node)
-
-	for _, i := range data {
-		m[i.ID] = Node{ID: i.ID}
-	}
-
-	rootNode := Node{}
+	m := make([]Node, len(data))
+	indices := make([]int, len(data))
 	rootNodeSet := false
 
-	for _, i := range data {
-		n, ok := m[i.ID]
-
-		if !ok {
-			return nil, errors.New("Could not find node")
+	for k, i := range data {
+		if i.ID >= len(data) || i.ID < 0 {
+			return nil, errors.New("Incorrect ID")
 		}
 
-		parent, ok := m[i.Parent]
-
-		if !ok {
-			return nil, errors.New("Could not find parent")
+		if i.ID < i.Parent || (i.ID == i.Parent && i.ID != 0) {
+			return nil, errors.New("Node id has to be strictly lower than the parent")
 		}
 
-		if i.Parent != i.ID {
-			parent.Children = append(parent.Children, &n)
-		} else {
+		if indices[i.ID] != 0 {
+			return nil, errors.New("Node set twice ")
+		}
+
+		m[i.ID] = Node{ID: i.ID}
+		indices[i.ID] = k
+
+		if i.ID == 0 {
 			if rootNodeSet {
-				return nil, errors.New("Can not have two root nodes")
+				return nil, errors.New("Root node set twice")
 			}
-			rootNode = n
 			rootNodeSet = true
+		}
+	}
+
+	if !rootNodeSet {
+		return nil, errors.New("No root node")
+	}
+
+	for index := range m {
+		n := &m[index]
+
+		parent := data[indices[index]].Parent
+
+		if parent != index {
+			m[parent].Children = append(m[parent].Children, n)
+			sort.Slice(m[parent].Children, func(i, j int) bool {
+				return m[parent].Children[i].ID < m[parent].Children[j].ID
+			})
 		}
 
 	}
 
-	return &rootNode, nil
+	return &m[0], nil
 }
