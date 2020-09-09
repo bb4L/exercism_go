68635015af33f85329fdcdd24fef95c1230d2748
bb4L <39266013+bb4L@users.noreply.github.com>
2020-09-09 17:54:41 +0200

    Add solution for tree-building

--- src/go/tree-building/tree_building.go
+++ src/go/tree-building/tree_building.go
@@ -23,36 +23,21 @@ func Build(data []Record) (*Node, error) {
 		return nil, nil
 	}
 
+	sort.SliceStable(data, func(i, j int) bool {
+		return data[i].ID < data[j].ID
+	})
+
 	m := make([]Node, len(data))
 	indices := make([]int, len(data))
-	rootNodeSet := false
 
 	for k, i := range data {
-		if i.ID >= len(data) || i.ID < 0 {
-			return nil, errors.New("Incorrect ID")
-		}
-
-		if i.ID < i.Parent || (i.ID == i.Parent && i.ID != 0) {
-			return nil, errors.New("Node id has to be strictly lower than the parent")
-		}
-
-		if indices[i.ID] != 0 {
-			return nil, errors.New("Node set twice ")
+		if i.ID != k || (i.ID < i.Parent || (i.ID == i.Parent && i.ID != 0)) || (indices[i.ID] != 0) {
+			return nil, errors.New("Incorrec structure")
 		}
 
 		m[i.ID] = Node{ID: i.ID}
 		indices[i.ID] = k
 
-		if i.ID == 0 {
-			if rootNodeSet {
-				return nil, errors.New("Root node set twice")
-			}
-			rootNodeSet = true
-		}
-	}
-
-	if !rootNodeSet {
-		return nil, errors.New("No root node")
 	}
 
 	for index := range m {
@@ -62,9 +47,6 @@ func Build(data []Record) (*Node, error) {
 
 		if parent != index {
 			m[parent].Children = append(m[parent].Children, n)
-			sort.Slice(m[parent].Children, func(i, j int) bool {
-				return m[parent].Children[i].ID < m[parent].Children[j].ID
-			})
 		}
 
 	}
