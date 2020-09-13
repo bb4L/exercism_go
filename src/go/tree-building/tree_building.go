1a2ae9a2a2de903de8ffc56caac7e067b87c3b12
bb4L <39266013+bb4L@users.noreply.github.com>
2020-09-13 10:38:30 +0200

    Feat/tree building (#6)

---
+++ src/go/tree-building/tree_building.go
@@ -0,0 +1,42 @@
+package tree
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Record represents a record to setup the tree
+type Record struct {
+	ID     int
+	Parent int
+}
+
+// Node a node for the tree
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+// Build a tree from the given records
+func Build(data []Record) (*Node, error) {
+	sort.Slice(data, func(i, j int) bool {
+		return data[i].ID < data[j].ID
+	})
+
+	m := map[int]*Node{}
+
+	for i, r := range data {
+		if r.ID != i || r.Parent > r.ID || r.ID > 0 && r.Parent == r.ID {
+			return nil, fmt.Errorf("bad record: %v (out of sequence or bad parent ID)", r)
+		}
+
+		m[r.ID] = &Node{ID: r.ID}
+
+		if r.ID > 0 {
+			parent := r.Parent
+			m[parent].Children = append(m[parent].Children, m[i])
+		}
+	}
+
+	return m[0], nil
+}
