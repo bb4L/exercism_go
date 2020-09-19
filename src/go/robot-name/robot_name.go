cf6f59d5945d8e7e1a0ec5482fdae20340de1426
bb4L <39266013+bb4L@users.noreply.github.com>
2020-09-19 20:49:35 +0200

    Add solution robot-name (#8)

---
+++ src/go/robot-name/robot_name.go
@@ -0,0 +1,57 @@
+package robotname
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Robot type for a robot
+type Robot struct {
+	name string
+}
+
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var names = make(map[string]bool)
+
+func nameExists(value string) bool {
+	_, ok := names[value]
+	return ok
+}
+
+func removeName(val string) {
+	// delete(names, val)
+}
+
+func createName() string {
+	r1 := random.Intn(26) + 'A'
+	r2 := random.Intn(26) + 'A'
+	num := random.Intn(1000)
+	return fmt.Sprintf("%c%c%03d", r1, r2, num)
+}
+
+// Name get the name of the robot
+func (r *Robot) Name() (string, error) {
+	if r.name != "" {
+		return r.name, nil
+	}
+	if len(names) == 26*26*10*10*10*10 {
+		return "", errors.New("Too many robots")
+	}
+	r.name = createName()
+	for names[r.name] {
+		r.name = createName()
+	}
+	names[r.name] = true
+	return r.name, nil
+}
+
+// Reset resets the name
+func (r *Robot) Reset() {
+	removeName(r.name)
+	r.name = ""
+}
