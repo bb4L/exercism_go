aa4773979bd7f90f5c392c2fa15b5f800b56875c
bb4L <39266013+bb4L@users.noreply.github.com>
2020-10-16 17:25:38 +0200

    Update solution for variable-length-quantity

--- src/go/variable-length-quantity/variable_length_quantity.go
+++ src/go/variable-length-quantity/variable_length_quantity.go
@@ -1,8 +1,11 @@
 package variablelengthquantity
 
+import "fmt"
+
 const (
-	bitmask   byte = 127
-	checkmask byte = 128
+	bitmask    = 127
+	checkmask  = 128
+	shiftvalue = 7
 )
 
 // EncodeVarint encode to []byte
@@ -10,12 +13,33 @@ func EncodeVarint(input []uint32) []byte {
 	var output []byte
 
 	for _, v := range input {
-		seq, v := []byte{byte(v) & bitmask}, v>>7
-		for ; v > 0; v = v >> 7 {
+		length := len(fmt.Sprintf("%b", v))
+		seq := make([]byte, length)
+		seq[length-1] = byte(v) & bitmask
+		v = v >> shiftvalue
+		k := length - 2
+
+		for ; v > 0; v = v >> shiftvalue {
 			val := byte(v)&bitmask + checkmask
-			seq = append([]byte{val}, seq...)
+			seq[k] = val
+			k--
 		}
-		output = append(output, seq...)
+
+		res := []byte{}
+		keepProceeding := false
+
+		for _, z := range seq {
+			if z != 0 || keepProceeding {
+				res = append(res, z)
+				keepProceeding = true
+			}
+		}
+
+		if len(res) == 0 {
+			res = []byte{byte(0)}
+		}
+
+		output = append(output, res...)
 	}
 
 	return output
@@ -27,8 +51,11 @@ func DecodeVarint(input []byte) ([]uint32, error) {
 	var val uint32 = 0
 
 	for _, v := range input {
+
 		seq = append(seq, uint32(v&bitmask))
+
 		if (v & checkmask) != checkmask {
+
 			for i, b := range seq {
 				shift := (len(seq) - i - 1) * 7
 				val += b << shift
