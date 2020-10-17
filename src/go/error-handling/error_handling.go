6641c6828f6232c9a3376466bc46e4302f30bb65
bb4L <39266013+bb4L@users.noreply.github.com>
2020-10-17 10:33:06 +0200

    Update solution for error-handling

--- src/go/error-handling/error_handling.go
+++ src/go/error-handling/error_handling.go
@@ -1,24 +1,21 @@
 package erratum
 
-import (
-	"fmt"
-)
-
 // Use wrapper to use a given opener with the input
 func Use(opener ResourceOpener, data string) (err error) {
 	var value Resource
 
-	for value, err = opener(); err != nil; value, err = opener() {
+	value, err = opener()
+
+	for err != nil {
 		if _, ok := err.(TransientError); !ok {
 			return err
 		}
+		value, err = opener()
 	}
 
 	defer value.Close()
 	defer func() {
 		if e := recover(); e != nil {
-			fmt.Println(e)
-
 			fErr, ok := e.(FrobError)
 			if ok {
 				value.Defrob(fErr.defrobTag)
@@ -27,6 +24,10 @@ func Use(opener ResourceOpener, data string) (err error) {
 			nErr, okE := e.(error)
 			if okE {
 				err = nErr
+			}
+
+			if !ok && !okE {
+
 			}
 		}
 	}()
