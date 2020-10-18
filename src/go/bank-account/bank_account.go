4a33a4e87ae840e6e797bf8608d1922470247b9d
bb4L <39266013+bb4L@users.noreply.github.com>
2020-10-18 10:18:56 +0200

    Update solution for bank-account

--- src/go/bank-account/bank_account.go
+++ src/go/bank-account/bank_account.go
@@ -4,9 +4,9 @@ import "sync"
 
 // Account bank account
 type Account struct {
-	open    bool
+	sync.Mutex
+	closed  bool
 	balance int64
-	mutex   *sync.Mutex
 }
 
 // Open opens an account with the given balance
@@ -14,46 +14,48 @@ func Open(initialDeposit int64) *Account {
 	if initialDeposit < 0 {
 		return nil
 	}
-	return &Account{true, initialDeposit, &sync.Mutex{}}
+	return &Account{sync.Mutex{}, false, initialDeposit}
 }
 
 // Close closes an account
 func (a *Account) Close() (payout int64, ok bool) {
-	a.mutex.Lock()
-	if !a.open {
+	a.Lock()
+	defer a.Unlock()
+
+	if a.closed {
 		payout, ok = 0, false
 	} else {
-		a.open = false
+		a.closed = true
 		payout, ok = a.balance, true
 	}
-
-	a.mutex.Unlock()
 	return
 }
 
 // Balance retrieves the balance of an account
 func (a *Account) Balance() (balance int64, ok bool) {
-	a.mutex.Lock()
-	if !a.open {
+	a.Lock()
+	defer a.Unlock()
+
+	if a.closed {
 		balance, ok = 0, false
 	} else {
 		balance, ok = a.balance, true
 	}
 
-	a.mutex.Unlock()
 	return
 }
 
 // Deposit deposits the given amount
 func (a *Account) Deposit(amount int64) (balance int64, ok bool) {
-	a.mutex.Lock()
-	if a.balance+amount < 0 || !a.open {
+	a.Lock()
+	defer a.Unlock()
+
+	if a.balance+amount < 0 || a.closed {
 		balance, ok = 0, false
 	} else {
 		a.balance += amount
 		balance, ok = a.balance, true
 	}
 
-	a.mutex.Unlock()
 	return
 }
