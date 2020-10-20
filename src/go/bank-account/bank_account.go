e282806a5e21d3d56b62a3d72cebc94360bf3061
bb4L <39266013+bb4L@users.noreply.github.com>
2020-10-20 16:11:53 +0200

    Update solution for bank-account

--- src/go/bank-account/bank_account.go
+++ src/go/bank-account/bank_account.go
@@ -18,45 +18,39 @@ func Open(initialDeposit int64) *Account {
 }
 
 // Close closes an account
-func (a *Account) Close() (payout int64, ok bool) {
+func (a *Account) Close() (int64, bool) {
 	a.Lock()
 	defer a.Unlock()
 
 	if a.closed {
-		payout, ok = 0, false
-		return
+		return 0, false
 	}
 
 	a.closed = true
-	payout, ok = a.balance, true
-	return
+	return a.balance, true
 }
 
 // Balance retrieves the balance of an account
-func (a *Account) Balance() (balance int64, ok bool) {
+func (a *Account) Balance() (int64, bool) {
 	a.RLock()
 	defer a.RUnlock()
 
 	if a.closed {
-		balance, ok = 0, false
-		return
+		return 0, false
 	}
 
-	balance, ok = a.balance, true
-	return
+	return a.balance, true
 }
 
 // Deposit deposits the given amount
-func (a *Account) Deposit(amount int64) (balance int64, ok bool) {
+func (a *Account) Deposit(amount int64) (int64, bool) {
 	a.Lock()
 	defer a.Unlock()
 
 	if a.balance+amount < 0 || a.closed {
-		balance, ok = 0, false
-		return
+		return 0, false
 	}
 
 	a.balance += amount
-	balance, ok = a.balance, true
-	return
+	return a.balance, true
 }
