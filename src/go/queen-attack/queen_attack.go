73fce3e84776d367d764336ba1fcb129c038ea9f
bb4L <39266013+bb4L@users.noreply.github.com>
2021-04-11 08:54:56 +0200

    Add solution queen-attack

---
+++ src/go/queen-attack/queen_attack.go
@@ -0,0 +1,19 @@
+package queenattack
+
+import (
+	"errors"
+)
+
+func CanQueenAttack(q1, q2 string) (bool, error) {
+	if q1 == q2 {
+		return false, errors.New("same position")
+	}
+	if invalidValue(q1) || invalidValue(q2) {
+		return false, errors.New("invalid position")
+	}
+	return q1[0] == q2[0] || q1[1] == q2[1] || (q1[0]-q2[0] == q1[1]-q2[1]) || (q1[0]-q2[0] == q2[1]-q1[1]), nil
+}
+
+func invalidValue(val string) bool {
+	return len(val) != 2 || (val[0] < 'a' || val[0] > 'h') || (val[1] < '1' || val[1] > '8')
+}
