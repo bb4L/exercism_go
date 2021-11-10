adc438cd098d8f80e2ea1bf58dc0ccefbb409333
L P <39266013+bb4L@users.noreply.github.com>
2021-11-10 20:05:42 +0100

    Add solution chessboard (#52)

---
+++ src/go/chessboard/chessboard.go
@@ -0,0 +1,52 @@
+package chessboard
+
+// Declare a type named Rank which stores if a square is occupied by a piece - this will be a slice of bools
+type Rank []bool
+
+// Declare a type named Chessboard contains a map of eight Ranks, accessed with values from "A" to "H"
+type Chessboard map[string]Rank
+
+// CountInRank returns how many squares are occupied in the chessboard,
+// within the given rank
+func CountInRank(cb Chessboard, rank string) (count int) {
+	for _, val := range cb[rank] {
+		if val {
+			count++
+		}
+	}
+	return
+}
+
+// CountInFile returns how many squares are occupied in the chessboard,
+// within the given file
+func CountInFile(cb Chessboard, file int) (count int) {
+	for _, val := range cb {
+		if file > len(val) {
+			continue
+		}
+		if val[file-1] {
+			count++
+		}
+	}
+	return
+}
+
+// CountAll should count how many squares are present in the chessboard
+func CountAll(cb Chessboard) (count int) {
+	for _, file := range cb {
+		count += len(file)
+	}
+	return
+}
+
+// CountOccupied returns how many squares are occupied in the chessboard
+func CountOccupied(cb Chessboard) (count int) {
+	for _, file := range cb {
+		for _, val := range file {
+			if val {
+				count++
+			}
+		}
+	}
+	return
+}
