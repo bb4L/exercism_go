70c8cf51d508312a060b53e828e60c8ab84298b3
L P <39266013+bb4L@users.noreply.github.com>
2021-11-15 20:59:11 +0100

    Add solution phone-number (#60)

---
+++ src/go/phone-number/phone_number.go
@@ -0,0 +1,74 @@
+package phonenumber
+
+import (
+	"errors"
+	"unicode"
+)
+
+func Number(phoneNumber string) (string, error) {
+	digits := []rune{}
+	for _, val := range phoneNumber {
+		if unicode.IsDigit(val) {
+			digits = append(digits, val)
+		} else {
+			if val == '(' || val == ')' || val == '-' || val == '.' || val == '+' || unicode.IsSpace(val) {
+				continue
+			}
+
+			if unicode.IsLetter(val) {
+				return "", errors.New("leters not permitted")
+			}
+
+			return "", errors.New("punctuations not permitted")
+		}
+	}
+
+	if len(digits) < 10 {
+		return "", errors.New("incorrect number of digits")
+	}
+
+	if len(digits) > 11 {
+		return "", errors.New("more than 11 digits")
+	}
+
+	if len(digits) == 11 {
+		if digits[0] != '1' {
+			return "", errors.New("11 digits must start with 1")
+		}
+		digits = digits[1:]
+	}
+
+	areaCode := digits[0]
+	if areaCode == '0' {
+		return "", errors.New("area code cannot start with zero")
+	}
+	if areaCode == '1' {
+		return "", errors.New("area code cannot start with one")
+	}
+
+	exchangeCode := digits[3]
+	if exchangeCode == '0' {
+		return "", errors.New("exchange code cannot start with zero")
+	}
+	if exchangeCode == '1' {
+		return "", errors.New("exchange code cannot start with one")
+	}
+	return string(digits), nil
+}
+
+func AreaCode(phoneNumber string) (string, error) {
+	phoneNumber, err := Number(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+	return phoneNumber[0:3], nil
+}
+
+func Format(phoneNumber string) (string, error) {
+	phoneNumber, err := Number(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+	// return fmt.Sprintf("(%s) %s-%s", phoneNumber[0:3], phoneNumber[3:6], phoneNumber[6:10]), nil
+	return "(" + phoneNumber[0:3] + ") " + phoneNumber[3:6] + "-" + phoneNumber[6:10], nil
+}
