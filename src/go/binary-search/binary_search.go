e47c0204004c4d34d3cdafa4c9270220e80ee390
bb4L <39266013+bb4L@users.noreply.github.com>
2021-11-19 00:12:54 +0100

    Update solution for binary-search

--- src/go/binary-search/binary_search.go
+++ src/go/binary-search/binary_search.go
@@ -1,44 +1,19 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	if len(list) == 0 {
-		return -1
-	}
-
 	startIdx := 0
-
-	if list[startIdx] == key {
-		return startIdx
-	}
-
 	endIdx := len(list) - 1
 
 	for startIdx <= endIdx {
-		if list[startIdx] > key {
-			return -1
-		}
-
 		middleIdx := (startIdx + endIdx) / 2
 		middle := list[middleIdx]
 
-		if middle == key {
-			return middleIdx
-		}
-
 		if key > middle {
 			startIdx = middleIdx + 1
-		}
-
-		if key < middle {
+		} else if key < middle {
 			endIdx = middleIdx - 1
-		}
-
-		if startIdx >= len(list) {
-			return -1
-		}
-
-		if list[startIdx] == key {
-			return startIdx
+		} else {
+			return middleIdx
 		}
 	}
 
