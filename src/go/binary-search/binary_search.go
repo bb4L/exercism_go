971413a973723572b25bd17ac2fd954a54082a22
L P <39266013+bb4L@users.noreply.github.com>
2021-11-20 20:31:21 +0100

    Add solution binary-search (#63)

---
+++ src/go/binary-search/binary_search.go
@@ -0,0 +1,21 @@
+package binarysearch
+
+func SearchInts(list []int, key int) int {
+	startIdx := 0
+	endIdx := len(list) - 1
+
+	for startIdx <= endIdx {
+		middleIdx := (startIdx + endIdx) / 2
+		middle := list[middleIdx]
+
+		if key > middle {
+			startIdx = middleIdx + 1
+		} else if key < middle {
+			endIdx = middleIdx - 1
+		} else {
+			return middleIdx
+		}
+	}
+
+	return -1
+}
