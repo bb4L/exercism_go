d583e3cc1ed0e2ad0d05dfbefe9be64bfc044e3b
bb4L <39266013+bb4L@users.noreply.github.com>
2021-11-22 22:16:04 +0100

    Update solution for vehicle-purchase

--- src/go/vehicle-purchase/vehicle_purchase.go
+++ src/go/vehicle-purchase/vehicle_purchase.go
@@ -2,25 +2,21 @@ package purchase
 
 import (
 	"fmt"
-	"sort"
+	"strings"
 )
 
 // NeedsLicense determines whether a license is need to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	switch kind {
-	case "car", "truck":
-		return true
-	default:
-		return false
-	}
+	return kind == "car" || kind == "truck"
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in dictionary order.
 func ChooseVehicle(option1, option2 string) string {
-	s := []string{option1, option2}
-	sort.Strings(s)
-	return fmt.Sprintf("%s is clearly the better choice.", s[0])
-
+	resString := option2
+	if strings.Compare(option1, option2) <= 0 {
+		resString = option1
+	}
+	return fmt.Sprintf("%s is clearly the better choice.", resString)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
