05e29783a0a8e787a33db30e47a0ab945aad064b
bb4L <39266013+bb4L@users.noreply.github.com>
2021-11-23 20:06:19 +0100

    Add solution house

---
+++ src/go/house/house.go
@@ -0,0 +1,39 @@
+package house
+
+import "strings"
+
+var steps = []string{"house that Jack built", "malt", "rat", "cat", "dog", "cow with the crumpled horn",
+	"maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn",
+	"rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"}
+
+var addendum = []string{"lay in", "ate", "killed", "worried", "tossed", "milked", "kissed",
+	"married", "woke", "kept", "belonged to"}
+
+func Verse(v int) string {
+	var sb strings.Builder
+	sb.WriteString("This is the ")
+	sb.WriteString(steps[v-1])
+	if v > 1 {
+		for i := v - 1; i > 0; i-- {
+			sb.WriteString("\nthat ")
+			sb.WriteString(addendum[i-1])
+			sb.WriteString(" the ")
+			sb.WriteString(steps[i-1])
+
+		}
+	}
+
+	sb.WriteString(".")
+	return sb.String()
+}
+
+func Song() string {
+	var sb strings.Builder
+	for i := 1; i < 13; i++ {
+		sb.WriteString(Verse(i))
+		if i < 12 {
+			sb.WriteString("\n\n")
+		}
+	}
+	return sb.String()
+}
