05753c295dc3b796c73cee4689ed6a379de6d45d
bb4L <39266013+bb4L@users.noreply.github.com>
2021-11-27 15:31:42 +0100

    Update solution for kindergarten-garden

--- src/go/kindergarten-garden/kindergarten_garden.go
+++ src/go/kindergarten-garden/kindergarten_garden.go
@@ -23,37 +23,47 @@ func NewGarden(plantsOriginal string, children []string) (*Garden, error) {
 		return &Garden{}, errors.New("garden format invalid")
 	}
 
+	var err error
+
 	allPlants := make(map[string][]string)
 	sortedChildren := make([]string, len(children))
 	copy(sortedChildren, children)
 	sort.Strings(sortedChildren)
 
-	for i, name := range sortedChildren {
+	for idx, name := range sortedChildren {
 		if _, ok := allPlants[name]; ok {
 			return &Garden{}, errors.New("duplicate name")
 		}
 
-		for _, plantLine := range plantLines[1:] {
-			if len(plantLine) != 2*len(children) {
-				return &Garden{}, errors.New("invalid format")
-			}
-			for _, plant := range plantLine[2*i : 2*i+2] {
-				if val, ok := plantLookup[plant]; ok {
-					allPlants[name] = append(allPlants[name], val)
-				} else {
-					return &Garden{}, errors.New("plants invalid")
-				}
-			}
+		allPlants, err = handlePlantline(idx, allPlants, children, name, plantLines)
+		if err != nil {
+			return nil, err
 		}
 	}
 	return &Garden{plants: allPlants}, nil
+}
+
+func handlePlantline(i int, allPlants map[string][]string, children []string, name string, plantLines []string) (map[string][]string, error) {
+	for _, plantLine := range plantLines[1:] {
+		if len(plantLine) != 2*len(children) {
+			return nil, errors.New("invalid format")
+		}
+		for _, plant := range plantLine[2*i : 2*i+2] {
+			val, ok := plantLookup[plant]
+			if !ok {
+				return nil, errors.New("plants invalid")
+			}
+			allPlants[name] = append(allPlants[name], val)
+		}
+	}
+	return allPlants, nil
 }
 
 func (g *Garden) Plants(child string) ([]string, bool) {
 	plants, ok := g.plants[child]
 
 	if !ok {
-		return []string{}, false
+		return nil, false
 	}
 
 	return plants, true
