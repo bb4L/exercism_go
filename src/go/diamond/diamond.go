960534ff3f8ef55769588918a8082e56c12142a5
L P <39266013+bb4L@users.noreply.github.com>
2021-11-28 07:18:26 +0100

    Add solution diamond (#34)

---
+++ src/go/diamond/diamond.go
@@ -0,0 +1,40 @@
+package diamond
+
+import (
+	"errors"
+	"strings"
+)
+
+func Gen(b byte) (string, error) {
+	if b < byte('A') || b > byte('Z') {
+		return "", errors.New("invalid character")
+	}
+
+	difference := int(b - 'A')
+
+	if difference == 0 {
+		return "A\n", nil
+	}
+	result := []string{getLine(difference, difference)}
+
+	for i := difference - 1; i >= 0; i-- {
+		line := getLine(difference, i)
+		result = append(append([]string{line}, result...), line)
+	}
+
+	return strings.Join(result, "\n") + "\n", nil
+}
+
+func getLine(totalDifference int, iteration int) string {
+	filler := " "
+	currentChar := string(byte('A' + iteration))
+	startEndSpace := strings.Repeat(filler, totalDifference-iteration)
+
+	if iteration == 0 {
+		startEndSpace = strings.Repeat(filler, totalDifference-iteration/2-(iteration%2))
+		return startEndSpace + currentChar + startEndSpace
+	}
+
+	return startEndSpace + currentChar + strings.Repeat(filler, iteration*2-1) + currentChar + startEndSpace
+
+}
