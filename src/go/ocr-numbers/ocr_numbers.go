a9ebfd23c1ed62f0f4b71af83bd650defc731556
bb4L <39266013+bb4L@users.noreply.github.com>
2021-12-01 21:14:55 +0100

    Update solution for ocr-numbers

--- src/go/ocr-numbers/ocr_numbers.go
+++ src/go/ocr-numbers/ocr_numbers.go
@@ -4,90 +4,25 @@ import (
 	"strings"
 )
 
+var digits = map[string]string{
+	" _ | ||_|   ": "0",
+	"     |  |   ": "1",
+	" _  _||_    ": "2",
+	" _  _| _|   ": "3",
+	"   |_|  |   ": "4",
+	" _ |_  _|   ": "5",
+	" _ |_ |_|   ": "6",
+	" _   |  |   ": "7",
+	" _ |_||_|   ": "8",
+	" _ |_| _|   ": "9",
+}
+
 func recognizeDigit(data []string) string {
-	if len(data) != 4 {
+	val, ok := digits[strings.Join(data, "")]
+	if !ok {
 		return "?"
 	}
-
-	switch data[0] {
-	case " _ ":
-		// can be 2,3,5,6,7,8,9,0
-		switch data[1] {
-		case " _|":
-			// 2,3
-			if data[2] == "|_ " {
-				return "2"
-			}
-			if data[2] == " _|" {
-				return "3"
-			}
-			return "?"
-
-		case "|_ ":
-			// 5,6
-			if data[2] == " _|" {
-				return "5"
-			}
-
-			if data[2] == "|_|" {
-				return "6"
-			}
-			return "?"
-
-		case "  |":
-			// 7
-			if data[2] != "  |" {
-				return "?"
-			}
-			return "7"
-
-		case "|_|":
-			// 8,9
-			if data[2] == "|_|" {
-				return "8"
-			}
-
-			if data[2] == " _|" {
-				return "9"
-			}
-			return "?"
-
-		case "| |":
-			// 0
-			if data[2] != "|_|" {
-				return "?"
-			}
-			return "0"
-
-		default:
-			return "?"
-		}
-
-	case "   ":
-		// can be 1,4
-		switch data[1] {
-		case "  |":
-			// 1
-			if data[2] != "  |" {
-				return "?"
-			}
-			return "1"
-
-		case "|_|":
-			//4
-			if data[2] != "  |" {
-				return "?"
-
-			}
-			return "4"
-
-		default:
-			return "?"
-		}
-
-	}
-
-	return "0"
+	return val
 }
 
 func Recognize(data string) []string {
@@ -98,11 +33,10 @@ func Recognize(data string) []string {
 	}
 
 	dataToCheck := splitted[1:]
-
 	result := []string{}
 
 	for line := 0; line <= len(splitted)-4; line += 4 {
-		lineResult := ""
+		var lineResult strings.Builder
 		for i := 0; i <= len(dataToCheck[line])-3; i += 3 {
 			numberData := []string{}
 
@@ -114,10 +48,10 @@ func Recognize(data string) []string {
 				numberData = append(numberData, dataToCheck[j+line][i:i+3])
 			}
 
-			lineResult += recognizeDigit(numberData)
+			lineResult.WriteString(recognizeDigit(numberData))
 		}
 
-		result = append(result, lineResult)
+		result = append(result, lineResult.String())
 	}
 
 	return result
