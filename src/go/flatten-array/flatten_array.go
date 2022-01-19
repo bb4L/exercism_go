bc7c0f85b3cdaf06bcf9977a3e0514dace896ea9
L P <39266013+bb4L@users.noreply.github.com>
2022-01-19 06:32:56 +0100

    Add solution flatten-array (#23)

---
+++ src/go/flatten-array/flatten_array.go
@@ -0,0 +1,28 @@
+package flatten
+
+import (
+	"reflect"
+)
+
+// Flatten flattens arbitrary nested arrays
+func Flatten(data interface{}) []interface{} {
+	result := []interface{}{}
+
+	for _, d := range data.([]interface{}) {
+		if d == nil {
+			continue
+		}
+		rt := reflect.TypeOf(d)
+		switch rt.Kind() {
+		case reflect.Slice:
+			res := Flatten(d.(interface{}))
+			if len(res) == 0 {
+				continue
+			}
+			result = append(result, res...)
+		default:
+			result = append(result, d)
+		}
+	}
+	return result
+}
